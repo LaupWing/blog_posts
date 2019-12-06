78452e7d3c5ab87929d9fe890f3aaca13fc4bad3
LaupWing <loc_strict@hotmail.com>
2019-12-06 14:52:56 +0100

    added the theme styling to the the menu nav

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 const Menu = () =>(
-    <nav className="menu"
-        style={{
-            display: 'flex',
-            justifyContent: 'center'
-        }}
-    >
+    <nav className="menu">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/blog">Blog</Link></li>
